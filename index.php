d84782b78b87b47b7a92136089a6cff8569a53ae
Shraddha Thakare <shraddhathakare75@gmail.com>
2025-06-19 11:26:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+    <link rel="stylesheet" href="indexStyle.css">
+</head>
+<form action="signUp.html">
+<body>
+    <div class="container">
+      <header>
+        <section>
+            <div class="head">
+                <marquee><h1>Welcome to BrainBusters</h1></marquee>
+            </div>
+        </section>
+        </header>
+
+        <section>
+            <div class="msg">
+                <p>Test Your Knowledge, <br/> Challenge Your Mind!</p>
+            </div>
+        </section>
+
+
+          <section>  
+            <div class="btn">
+                <button>Ready to start?</button>
+ 
+            </div>
+        </section>           
+      
+    </div>
+</body>
+</form>
+</html>
+
+
+
